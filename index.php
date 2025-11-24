c404f616a7d46ba1cdb1b17b8181d019aa0e3443
4lrxx <4lrxx>
2025-11-24 19:36:12 -0400

    Agregue el Stylo

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+    <div class="card" style="width: 14rem;">
+        <img src="img/alexander.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Alexander Parraz</h5>
+            <p class="card-text">Creo la página de “Bienvenido” después del login.</p>
+           
+        </div>
+    </div>
