aaa887bd4d206149eb0dcfbbd33c563671d19df5
4lrxx <4lrxx>
2025-11-24 18:32:58 -0400

    Agregue el Require

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+require('conexion.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+
     
 </body>
 </html>
